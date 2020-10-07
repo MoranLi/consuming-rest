867c41bf60cc3f9e61700ef08b5b3d91958d2595
Yukun Li <moran.li@usask.ca>
2020-10-06 20:49:03 -0600

    Initial commit

---
+++ src/main/java/com/example/consumingrest/Reply.java
@@ -0,0 +1,36 @@
+package com.example.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Reply {
+    private String type;
+    private Value value;
+
+    public Reply() {
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Reply{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/consumingrest/Value.java
@@ -0,0 +1,33 @@
+package com.example.consumingrest;
+
+public class Value {
+    private int id;
+    private String quote;
+
+    public Value() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    @Override
+    public String toString() {
+        return "Value{" +
+                "id=" + id +
+                ", quote='" + quote + '\'' +
+                '}';
+    }
+}
