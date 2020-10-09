bd64887ab67bf92ef7aa360b1b88a56f89d3457f
Yukun Li <moran.li@usask.ca>
2020-10-08 21:52:18 -0600

    switch to use mysql store / get data

--- src/main/java/com/example/consumingrest/ConsumingRestApplication.java
+++ src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -1,17 +1,33 @@
 package com.example.consumingrest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Timer;
 
 @SpringBootApplication
-public class ConsumingRestApplication {
+public class ConsumingRestApplication  implements CommandLineRunner {
+
+	@Autowired
+	ReplyTimer timer;
+
+	@Autowired
+	JdbcComponent component;
+
+	@Override
+	public void run(String... strings) throws Exception {
+		JdbcTemplate template = component.getTemplate();
+		template.execute("DROP TABLE IF EXISTS reply");
+		template.execute("CREATE TABLE reply (pid SERIAL, type VARCHAR(255), id INT, quote VARCHAR(255))");
+
+		timer.startTimer();
+	}
 
 	public static void main(String[] args) {
-		Timer timer = new Timer();
-		ReplyTimerTask myTask = new ReplyTimerTask();
-		timer.schedule(myTask, 5000L, 5000L);
 		SpringApplication.run(ConsumingRestApplication.class, args);
 	}
 
--- src/main/java/com/example/consumingrest/ReplyGet.java
+++
@@ -1,4 +0,0 @@
-package com.example.consumingrest;
-
-public class ReplyGet {
-}
